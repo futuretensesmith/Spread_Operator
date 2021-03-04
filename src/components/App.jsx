66822253a8c8d223639f48af67cc8f3edc841ab5
Kevin Smith <futuretense@gmail.com>
2021-03-03 17:19:54 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+// below using spread operator to add fullName object to user object
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   userName: "jamesbond007"
+// }
+
+// console.log(user)
